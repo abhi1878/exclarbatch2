c4ce90c446f7e00ecdbeca1a65fff8296dd37493
abhi1878 <abhishekyama1878@gmail.com>
2025-01-03 09:49:21 +0530

    Add files via upload

---
+++ java/basic/user.java
@@ -0,0 +1,9 @@
+public class user{
+    public static void main(string[] args){
+str username = "user 001";
+str pd = "user_001"
+while(username == "user 001") && (pd == "user_001") {
+System.out.println(x:"hello hi");
+}
+}
+}
