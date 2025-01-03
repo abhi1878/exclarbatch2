bf308524aa875b6d202996f67a4e8577cf6609f7
abhi1878 <abhishekyama1878@gmail.com>
2025-01-03 10:19:06 +0530

    Add files via upload

---
+++ java/basic/java/basic/abhiuser.java
@@ -0,0 +1,13 @@
+package CoreJava;
+
+public class User {
+    public static void main(String[] args) {
+        String username = "abhi";
+        String pd = "abhi001";
+
+        while (username.equals("abhi") && pd.equals("abhi001")) {
+            System.out.println("hello abhi");
+            break;
+        }
+    }
+}
---
+++ java/basic/java/basic/user.java
@@ -0,0 +1,9 @@
+public class user{
+    public static void main(String[] args) {
+String username = "user001";
+String pd = "user001"
+while(username == "user001") && (pd == "user_001") {
+System.out.println(x:"hello hi");
+}
+}
+}
