b0de3db068adc6432b5bb33fddd2df05b7ada44f
abhi1878 <abhishekyama1878@gmail.com>
2025-01-21 10:19:26 +0530

    Add files via upload

---
+++ java/basic/demo032.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+public class Demo032 {
+    public static void main(String[] args) {
+        // Create a HashSet to store string values
+        HashSet<String> stringSet = new HashSet<>();
+
+        // Add string values to the HashSet
+        stringSet.add("apple");
+        stringSet.add("banana");
+        stringSet.add("cherry");
+
+        // Print the HashSet
+        System.out.println("HashSet: " + stringSet);
+    }
+}
